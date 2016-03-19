ae2a35ddb05ba9d6cb1ab96a5ecbbbfabe3a2f03
Elfrink <b.p.j.elfrink@student.tue.nl>
2016-03-19 12:49:42 +0100

    Starting to work on the server requests

---
+++ app/src/main/java/com/example/s135123/kitchener/Favorites.java
@@ -0,0 +1,30 @@
+package com.example.s135123.kitchener;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * Created by s142451 on 16-3-2016.
+ */
+public class Favorites extends android.support.v4.app.Fragment {
+    private TextView textView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.favorites_fragment, container, false);
+    }
+
+    // Setting up the async server requests
+    
+
+    // List favorite meals
+
+}
---
+++ app/src/main/java/com/example/s135123/kitchener/Settings.java
@@ -0,0 +1,22 @@
+package com.example.s135123.kitchener;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by s142451 on 16-3-2016.
+ */
+public class Settings extends android.support.v4.app.Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.settings_fragment, container, false);
+    }
+
+    // Change standard amount of days for schedule
+
+    // Remove all favorites
+
+}
