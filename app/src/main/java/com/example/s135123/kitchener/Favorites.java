3e2fe19777efd79b644ebf39d4ae9c3221aecc5c
Elfrink <b.p.j.elfrink@student.tue.nl>
2016-03-21 17:34:32 +0100

    The images are included for sure now

---
+++ app/src/main/java/com/example/s135123/kitchener/Favorites.java
@@ -0,0 +1,40 @@
+package com.example.s135123.kitchener;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+
+/**
+ * Created by s142451 on 16-3-2016.
+ */
+public class Favorites extends android.support.v4.app.Fragment {
+    private TextView textView;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.favorites_fragment, container, false);
+    }
+
+
+
+    // List favorite meals
+
+}
