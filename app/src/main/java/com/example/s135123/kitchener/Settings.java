1e2450b61fb7c8cb30960198adac03e2e4ef3aff
Elfrink <b.p.j.elfrink@student.tue.nl>
2016-03-22 11:35:55 +0100

    Working on the settings fragment

--- app/src/main/java/com/example/s135123/kitchener/Settings.java
+++ app/src/main/java/com/example/s135123/kitchener/Settings.java
@@ -2,16 +2,23 @@ package com.example.s135123.kitchener;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 /**
  * Created by s142451 on 16-3-2016.
  */
 public class Settings extends android.support.v4.app.Fragment {
+    CheckBox ch1;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        ch1=(CheckBox)getView().findViewById(R.id.measure);
         return inflater.inflate(R.layout.settings_fragment, container, false);
     }
 
