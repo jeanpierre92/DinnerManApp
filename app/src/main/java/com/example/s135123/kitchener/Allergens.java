dd4b4e81ad285595dccf9b1594a608b4a863e8e9
unknown <s130604@S130604.campus.tue.nl>
2016-03-30 12:03:47 +0200

    allergen werkt nu met de server

--- app/src/main/java/com/example/s135123/kitchener/Allergens.java
+++ app/src/main/java/com/example/s135123/kitchener/Allergens.java
@@ -1,15 +1,22 @@
 package com.example.s135123.kitchener;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -40,7 +47,7 @@ public class Allergens extends AppCompatActivity implements View.OnClickListener
         button_addAllergen = (Button) findViewById(R.id.button_addAllergen);
         button_removeAllergen = (Button) findViewById(R.id.button_removeAllergen);
         editText_allergens = (EditText) findViewById(R.id.editText_allergens);
-
+        System.out.println("numAllergies: "+user.getAllergies().size());
         adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
                 android.R.id.text1,
@@ -84,16 +91,85 @@ public class Allergens extends AppCompatActivity implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
+        String allergen = editText_allergens.getText().toString();
+        System.out.println("ALLERGEN:"+allergen);
         switch (v.getId()) {
             case R.id.button_addAllergen:
-                addAllergens(editText_allergens.getText().toString());
+                addAllergens(allergen);
+                new AllergensTask(allergen).execute(true);
                 break;
             case R.id.button_removeAllergen:
-                removeAllergens(editText_allergens.getText().toString());
+                removeAllergens(allergen);
+                new AllergensTask(allergen).execute(false);
                 break;
             default:
                 updateList();
                 break;
+        }
+    }
+    private class AllergensTask extends AsyncTask<Boolean, Void, String> {
+        String allergen;
+
+        public AllergensTask(String allergen) {
+            this.allergen = allergen;
+        }
+
+        @Override
+        protected String doInBackground(Boolean... params) {
+            User user = User.getInstance();
+            //String authTokenUrl = "http://appdev-gr1.win.tue.nl:8008/api/authenticate/test/test123";
+            String authTokenUrl = "http://appdev-gr1.win.tue.nl:8008/api/authenticate/" + user.getUsername() + "/" + user.getPassword();
+            JSONObject authTokenJson = null;
+            SendRequest sendRequest = new SendRequest();
+            try {
+                authTokenJson = new JSONObject(sendRequest.sendGetRequest(authTokenUrl));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (authTokenJson == null) {
+                //something went wrong
+                return "failed";
+            }
+            String authToken = null;
+            try {
+                authToken = authTokenJson.getString("authToken");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (params[0]) {
+                //add to allergens
+                String addToAllergensUrl = "http://appdev-gr1.win.tue.nl:8008/api/user/" + user.getUsername() + "/" + authToken + "/addAllergens";
+                int statusCode = sendRequest.sendPostRequest(addToAllergensUrl, allergen);
+                if (statusCode != 200) {
+                    return "failed";
+                } else {
+                    if (!user.getAllergies().contains(allergen)) {
+                        user.addAllergy(allergen);
+                    }
+                }
+            } else {
+                //remove from favorites
+                String addToAllergensUrl = "http://appdev-gr1.win.tue.nl:8008/api/user/" + user.getUsername() + "/" + authToken + "/deleteAllergens";
+                int statusCode = sendRequest.sendDeleteRequest(addToAllergensUrl, allergen);
+                System.out.println("allergens delete url: "+addToAllergensUrl);
+                System.out.println("allergens statuscode: "+statusCode);
+                if (statusCode != 200) {
+                    return "failed";
+                } else {
+                    user.removeAllergy(allergen);
+                }
+            }
+            return "success";
+        }
+
+        @Override
+        protected void onPostExecute(final String result) {
+            if (result.equals("failed")) {
+                Toast toast = Toast.makeText(Allergens.this, "Unable to reach the server to modify allergens", Toast.LENGTH_LONG);
+                toast.show();
+            } else {
+                adapter.notifyDataSetChanged();
+            }
         }
     }
 }
